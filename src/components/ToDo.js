c15a84255e0c8375675dbbc4600f63ccaeb847a3
Marka Rush <markabodine@gmail.com>
2018-02-19 21:51:19 -0600

    Checkpoint React To Do States and Props

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li>Here is a todo.</li>
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span>{ this.props.description }</span>
+      </li>
     );
   }
 }
